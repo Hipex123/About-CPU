270355ae2c05b6d95a7f727ab8489463b2e51834
Hipex123 <117768734+Hipex123@users.noreply.github.com>
2024-11-18 19:19:38 +0100

    Bug fix for image rendering

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: '/About-CPU',
   images: {
     unoptimized: true,
   },
--- src/app/multimedia/page.tsx
+++ src/app/multimedia/page.tsx
@@ -6,10 +6,10 @@ export default function Multimedia() {
             <h1>Multimedia</h1>
           
             <Image src="/cpu1.jpg" alt="CPU image 1" width={700} height={500} />
-            <Image src="/cpu2.jpg" alt="CPU image 1" width={700} height={500} />
-            <Image src="/cpu3.jpg" alt="CPU image 1" width={700} height={500} />
-            <Image src="/cpu4.jpg" alt="CPU image 1" width={700} height={500} />
-            <Image src="/cpu5.jpg" alt="CPU image 1" width={700} height={500}/>
+            <Image src="/cpu2.jpg" alt="CPU image 2" width={700} height={500} />
+            <Image src="/cpu3.jpg" alt="CPU image 3" width={700} height={500} />
+            <Image src="/cpu4.jpg" alt="CPU image 4" width={700} height={500} />
+            <Image src="/cpu5.jpg" alt="CPU image 5" width={700} height={500}/>
         </main>
     );
 }
