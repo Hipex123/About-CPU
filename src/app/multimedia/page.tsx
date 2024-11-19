101695a11db4991b1b1ab3836e69382bb8f1d440
Hipex123 <117768734+Hipex123@users.noreply.github.com>
2024-11-19 07:52:12 +0100

    Style changes

--- src/app/multimedia/page.tsx
+++ src/app/multimedia/page.tsx
@@ -4,12 +4,13 @@ export default function Multimedia() {
     return (
         <main>
             <h1>Multimedia</h1>
-          
-            <Image src="./cpu1.jpg" alt="CPU image 1" width={700} height={500} />
-            <Image src="./cpu2.jpg" alt="CPU image 2" width={700} height={500} />
-            <Image src="./cpu3.jpg" alt="CPU image 3" width={700} height={500} />
-            <Image src="./cpu4.jpg" alt="CPU image 4" width={700} height={500} />
-            <Image src="./cpu5.jpg" alt="CPU image 5" width={700} height={500}/>
+            <div className="imageContainer">
+                <Image src="./cpu1.jpg" alt="CPU image 1" layout="responsive" width={700} height={500} />
+                <Image src="./cpu2.jpg" alt="CPU image 2" layout="responsive" width={700} height={500} />
+                <Image src="./cpu3.jpg" alt="CPU image 3" layout="responsive" width={700} height={500} />
+                <Image src="./cpu4.jpg" alt="CPU image 4" layout="responsive" width={700} height={500} />
+                <Image src="./cpu5.jpg" alt="CPU image 5" layout="responsive" width={700} height={500} />
+            </div>
         </main>
     );
 }
