ab1e2dbbfdf18eb631dcfbd399e839ebe156a9d4
Hipex123 <117768734+Hipex123@users.noreply.github.com>
2024-11-19 21:26:37 +0100

    Updated global css

--- src/app/cpu/page.tsx
+++ src/app/cpu/page.tsx
@@ -16,7 +16,7 @@ export default function CPUDescription() {
 
             <section>
                 <h2>Aritmična in logična enota (ALU – Arithmetic and Logic Unit)</h2>
-                <p>
+                <p className="bubbleP">
                     V aritmetični in logični enoti poteka vsa matematična in logična obdelava. Izvaja osnovne aritmetične 
                     operacije, kot so seštevanje, odštevanje, množenje in deljenje, ter logične operacije, kot so primerjave 
                     (večje od, manjše od, enako) in bitne funkcije (AND, OR, NOT, XOR). ALU je bistvenega pomena za izvajanje 
@@ -28,7 +28,7 @@ export default function CPUDescription() {
 
             <section>
                 <h2>Predpomnilnik (Cache)</h2>
-                <p>
+                <p className="bubbleP">
                     Predpomnilnik je specializirana vrsta hitrega pomnilnika, ki se nahaja v bližini procesorja. Deluje kot 
                     začasno območje za shranjevanje pogosto dostopnih podatkov in ukazov, kar bistveno skrajša čas, ki ga 
                     procesor potrebuje za pridobivanje podatkov iz počasnejšega glavnega pomnilnika (RAM). Običajno je več 
@@ -40,7 +40,7 @@ export default function CPUDescription() {
 
             <section>
                 <h2>Registri (Registers)</h2>
-                <p>
+                <p className="bubbleP">
                     Registri so majhna, hitra pomnilniška mesta, vgrajena neposredno v procesor. V njih so začasno shranjeni podatki, 
                     naslovi ali navodila, s katerimi procesor aktivno dela. Za razliko od običajnega pomnilnika so registri izjemno hitri, 
                     vendar omejeni po velikosti, zato so odlični za shranjevanje vrednosti, ki so takoj potrebne za izračune ali izvajanje ukazov. 
@@ -52,7 +52,7 @@ export default function CPUDescription() {
 
             <section>
                 <h2>Zaključek</h2>
-                <p>
+                <p className="bubbleP">
                     Struktura centralne procesne enote (CPU) je ključnega pomena za razumevanje učinkovitega izvajanja nalog računalnikov. 
                     Vsaka komponenta, od krmilne enote do enote ALU, registrov in predpomnilnika, ima ključno vlogo pri obdelavi ukazov in 
                     upravljanju podatkov. Te komponente skupaj zagotavljajo nemoteno izvajanje operacij, kar računalnikom omogoča hitro in 
